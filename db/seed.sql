010917c68ccd787f40b3054b60496d5fd935fab7
Tom Latimer <tlati083@uottawa.ca>
2024-07-29 16:55:45 -0400

    Change seeded patients to have proper phone numbers

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.patient (first_name, last_name, email, phone, home_address, dob, medicine, blood_type) VALUES
-('John', 'Doe', 'john.doe@example.com', '555-1234', '123 Elm Street', '1980-07-15', 'Ibuprofen', 'A+'),
-('Jane', 'Smith', 'jane.smith@example.com', '555-5678', '456 Oak Street', '1992-11-30', 'Acetaminophen', 'O-'),
-('Alice', 'Johnson', 'alice.johnson@example.com', '555-8765', '789 Pine Street', '1975-05-20', 'Amoxicillin', 'B+'),
-('Bob', 'Brown', 'bob.brown@example.com', '555-4321', '321 Maple Street', '1988-08-08', 'Metformin', 'AB-'),
-('Carol', 'Davis', 'carol.davis@example.com', '555-9876', '654 Cedar Street', '1995-12-25', 'Lisinopril', 'A-');
+('John', 'Doe', 'john.doe@example.com', '613 555-1234', '123 Elm Street', '1980-07-15', 'Ibuprofen', 'A+'),
+('Jane', 'Smith', 'jane.smith@example.com', '613 555-5678', '456 Oak Street', '1992-11-30', 'Acetaminophen', 'O-'),
+('Alice', 'Johnson', 'alice.johnson@example.com', '613 555-8765', '789 Pine Street', '1975-05-20', 'Amoxicillin', 'B+'),
+('Bob', 'Brown', 'bob.brown@example.com', '613 555-4321', '321 Maple Street', '1988-08-08', 'Metformin', 'AB-'),
+('Carol', 'Davis', 'carol.davis@example.com', '613 555-9876', '654 Cedar Street', '1995-12-25', 'Lisinopril', 'A-');
